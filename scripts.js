54a9b52adebeaefeb950abff5890899f699a7bff
JetLiTheQT <ekkachai.ittihrit2@gmail.com>
2023-03-19 07:44:54 -0700

    added js to make the benefits cards seem more responsive

--- scripts.js
+++ scripts.js
@@ -27,3 +27,26 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+const cards = document.querySelectorAll('.benefit-card-js');
+
+cards.forEach((card) => {
+  card.addEventListener('mousemove', (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const dx = (x - centerX) / centerX;
+    const dy = (y - centerY) / centerY;
+
+    const rotateY = dx * 10;
+    const rotateX = -dy * 10;
+
+    card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = '';
+  });
+});
